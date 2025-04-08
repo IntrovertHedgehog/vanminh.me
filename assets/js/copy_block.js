2a74039ad9445e3cd2860778a949b83c1b215225
IntrovertHedgehog <larby2409@gmail.com>
2025-04-09 02:14:30 +0800

    first post

--- assets/js/copy_block.js
+++ assets/js/copy_block.js
@@ -1,27 +1,27 @@
-let codeBlocks = document.querySelectorAll('div.highlight');
+let codeBlocks = document.querySelectorAll("div.highlight");
 
-let copyBtn = document.createElement('template')
+let copyBtn = document.createElement("template");
 copyBtn.innerHTML = `
 <div class="clip-container">
   <div class="clip-btn">
     <i id="ic" class="fa-regular fa-clone"></i>
   </div>
 </div>
-`
+`;
 
 codeBlocks.forEach(function (codeBlock) {
   var copyButton = copyBtn.content.children[0].cloneNode(true);
   codeBlock.append(copyButton);
 
-  copyButton.addEventListener('click', function () {
-    var code = codeBlock.querySelector('code').innerText.trim();
+  copyButton.addEventListener("click", function () {
+    var code = codeBlock.querySelector("code").innerText.trim();
     window.navigator.clipboard.writeText(code);
 
-    copyButton.querySelector('#ic').className = "fa-solid fa-check"
-    var fourSeconds = 700;
+    copyButton.querySelector("#ic").className = "fa-solid fa-check";
+    var t = 700;
 
     setTimeout(function () {
-      copyButton.querySelector('#ic').className = "fa-regular fa-clone"
-    }, fourSeconds);
+      copyButton.querySelector("#ic").className = "fa-regular fa-clone";
+    }, t);
   });
 });
